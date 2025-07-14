ed5063980efde5d557e3e4e450e4c54b75b09ecf
Shatukh <shatukh@gmail.com>
2025-07-14 16:14:43 +0100

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,1 +1,28 @@
+"""
+Pydantic-модели для валидации данных и определения схемы API.
+"""
+from pydantic import BaseModel
+from typing import List
+import uuid
 
+# Эти модели гарантируют, что ответ API всегда будет иметь правильную структуру.
+
+class Airport(BaseModel):
+    code: str
+    city: str
+
+class Price(BaseModel):
+    value: float
+    currency: str
+
+class Deal(BaseModel):
+    id: uuid.UUID
+    departureAirport: Airport
+    arrivalAirport: Airport
+    departureDate: str
+    returnDate: str
+    durationDays: int
+    price: Price
+
+class DealResponse(BaseModel):
+    data: List[Deal]
